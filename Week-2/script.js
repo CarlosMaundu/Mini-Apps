a9f38bb808915f5bf1ea9a1725768fb45656db44
carlos <carlmaundu@gmail.com>
2024-03-07 09:26:19 +0300

    Implemented event listener for form submission

--- Week-2/script.js
+++ Week-2/script.js
@@ -1,2 +1,8 @@
 const form = document.getElementById('akan-name-form');
+const birthdayInput = document.getElementById('birthday');
+const genderSelect = document.getElementById('gender');
+const resultDiv = document.getElementById('result');
 
+form.addEventListener('submit', function(event) {
+
+});
