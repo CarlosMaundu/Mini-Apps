ab10bd7f23cf03fb93a8180e7cf56778b52b232c
carlos <carlmaundu@gmail.com>
2024-04-18 16:40:36 +0300

    Additional Tests Scenarios

--- Week-4-assignment/TriangleTracker/tests/triangle.test.js
+++ Week-4-assignment/TriangleTracker/tests/triangle.test.js
@@ -18,4 +18,20 @@ describe('Triangle Type', () => {
     expect(triangleType(8,6,10)).toBe('Scalene');
     expect(triangleType(12,5,8)).toBe('Scalene');
   });
+
+  test('should correctly determine whether three lengths can make a right triangle', () => {
+    expect(triangleType(3,4,5)).toBe('Scalene');
+    expect(triangleType(5,12,13)).toBe('Scalene');
+    expect(triangleType(8,15,17)).toBe('Scalene');
+  });
+
+  test('should correctly determine whether three lengths can make a triangle when all parameters are zero', () => {
+    expect(triangleType(0, 0, 0)).toBe('Not a triangle');
+  });
+
+  test('should correctly determine whether three lengths can make a triangle when one parameter is a decimal number', () => {
+    expect(triangleType(3.5, 4, 5)).toBe('Scalene');
+    expect(triangleType(5.5, 5.5, 3)).toBe('Isosceles');
+    expect(triangleType(2.5, 2.5, 2.5)).toBe('Equilateral');
+  });
 });
