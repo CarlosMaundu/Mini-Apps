4a75d11684a9bbfbfebbf753f1ceb5914216ae17
carlos <carlmaundu@gmail.com>
2024-04-18 16:33:03 +0300

    Additional Tests Scenarios

--- Week-4-assignment/TriangleTracker/tests/triangle.test.js
+++ Week-4-assignment/TriangleTracker/tests/triangle.test.js
@@ -6,4 +6,16 @@ describe('Triangle Type', () => {
     expect(triangleType(3,3,3)).toBe('Equilateral');
     expect(triangleType(2,2,8)).toBe('Not a triangle');
   });
+
+  test('should correctly determine whether three lengths can make an isosceles triangle', () => {
+    expect(triangleType(5,5,3)).toBe('Isosceles');
+    expect(triangleType(3,5,5)).toBe('Isosceles');
+    expect(triangleType(5,3,5)).toBe('Isosceles');
+  });
+
+  test('should correctly determine whether three lengths can make a scalene triangle', () => {
+    expect(triangleType(7,9,10)).toBe('Scalene');
+    expect(triangleType(8,6,10)).toBe('Scalene');
+    expect(triangleType(12,5,8)).toBe('Scalene');
+  });
 });
